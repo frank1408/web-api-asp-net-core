a5a679cf15526278493a040f56b28b73c41dc709
Franklin Rodriguez <frank14082013@gmail.com>
2022-05-02 21:42:08 -0600

    get, post, put, delete

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -11,6 +11,9 @@ namespace Projects.Controllers
 	{
 		public PizzaController()
 		{
+
+			// https://docs.microsoft.com/en-us/learn/modules/build-web-api-aspnet-core/7-crud
+
 		}
 
 		// crud create read update delete
@@ -35,8 +38,42 @@ namespace Projects.Controllers
 		}
 
 
+		[HttpPost]
+		public IActionResult Create(Pizza new_pizza)
+        {
+			PizzaService.Add(new_pizza);
+			return CreatedAtAction( nameof(Create) , new { id=new_pizza.Id }, new_pizza );
+        }
 
 
+		[HttpPut("{id}")]
+		public IActionResult Update(int id_pizza, Pizza updated_pizza)
+		{
+            if ( id_pizza != updated_pizza.Id )
+            {
+				return BadRequest();
+            }
+			var already_exist_pizza = PizzaService.Get(id_pizza);
+			if (already_exist_pizza == null) // if (already_exist_pizza is null)
+			{
+				return NotFound();
+            }
+			PizzaService.Update(updated_pizza);
+			return NoContent();
+		}
+
+
+		[HttpDelete]
+		public IActionResult Delete(int id_pizza)
+		{
+			var already_exist_pizza = PizzaService.Get(id_pizza);
+			if(already_exist_pizza == null)
+            {
+				return NotFound();
+            }
+			PizzaService.Delete(id_pizza);
+			return NoContent();
+		}
 
 
 
